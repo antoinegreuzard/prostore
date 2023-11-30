0786de229ea3f2f2e60fadb6e79ec4ea7955ab41
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-11-30 15:40:22 +0100

    chore: delete color filter

--- src/pages/item/[slug].js
+++ src/pages/item/[slug].js
@@ -71,7 +71,7 @@ const Item = ({ itemInfo, categoriesGroup, navigationItems }) => {
             <h1 className={cn('h3', styles.title)}>{itemInfo[0]?.title}</h1>
             <div className={styles.cost}>
               <div className={cn('status-stroke-green', styles.price)}>
-                {`$${itemInfo[0]?.metadata?.price}`}
+                {`${itemInfo[0]?.metadata?.price} €`}
               </div>
               <div className={styles.counter}>
                 {itemInfo[0]?.metadata?.count > 0
