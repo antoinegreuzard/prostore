3b42b0228df0a2634475ee4abb344006393d4379
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-12-09 11:08:47 +0100

    Fix card clickability issue in UploadDetails/Cards component
    Modifies the `Cards` component in `src/screens/UploadDetails/Cards/index.js` to fix an issue where the cards were not clickable.
    
    Changes:
    - Adds missing `cn` function to the `className` props in the `div` element.
    - Adds `onKeyPress`, `role`, and `tabIndex` props to make the cards keyboard accessible.
    
    Issue fixed: clicking on the cards now triggers the `handleChoose` function.
    
    Note: Ignores changes to imports and requires.

--- src/screens/UploadDetails/Cards/index.js
+++ src/screens/UploadDetails/Cards/index.js
@@ -7,20 +7,22 @@ function Cards({
   className, items, handleChoose, category,
 }) {
   return (
-    <div className={(className, styles.cards)}>
-      {items
-        && Object.entries(items)?.map((x, index) => (
-          <div
-            className={cn(styles.card, x[0] === category && styles.active)}
-            key={index}
-            onClick={() => handleChoose(x[0])}
-          >
-            <div className={styles.plus}>
-              <Icon name="plus" size="24" />
-            </div>
-            <div className={styles.subtitle}>{x[1]}</div>
+    <div className={cn(className, styles.cards)}>
+      {items && Object.entries(items)?.map((x, index) => (
+        <div
+          className={cn(styles.card, x[0] === category && styles.active)}
+          key={index}
+          onClick={() => handleChoose(x[0])}
+          onKeyPress={() => handleChoose(x[0])}
+          role="button"
+          tabIndex={0}
+        >
+          <div className={styles.plus}>
+            <Icon name="plus" size="24" />
           </div>
-        ))}
+          <div className={styles.subtitle}>{x[1]}</div>
+        </div>
+      ))}
     </div>
   );
 }
