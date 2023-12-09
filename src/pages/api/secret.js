5ef3dec989bb3f3a70d2b3cc6661ac9d2048e0d9
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-12-09 12:59:32 +0100

    Refactor secret API session handling
    Previously, the `haveSecret` higher-order function returned a 401 status code and an error message if the user was not authenticated. Now, it sets the response status to 401, sends the error message as JSON, and explicitly returns `undefined` afterwards. Additionally, there is explicit `undefined` return after handling the original handler and catching any errors. This refactoring improves the clarity and readability of the code.

--- src/pages/api/secret.js
+++ src/pages/api/secret.js
@@ -4,19 +4,21 @@ import { APP_KEY } from '../../utils/constants/appConstants';
 
 require('dotenv').config();
 
-// The haveSecret higher-order function
 const haveSecret = (handler) => withIronSessionApiRoute(async (req, res) => {
   try {
     const { user } = req.session;
     if (!user) {
-      return res.status(401).json({ error: 'Accès refusé' });
+      res.status(401).json({ error: 'Accès refusé' });
+      return undefined; // Explicitly return undefined
     }
     req.userId = user.id;
 
     // Call the original handler now that we've handled the session check
     await handler(req, res);
+    return undefined; // Explicitly return undefined
   } catch (error) {
     res.status(error.status || 500).json({ error: error.message });
+    return undefined; // Explicitly return undefined
   }
 }, {
   cookieName: APP_KEY,
