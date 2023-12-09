575aa91897587851ceae4610a3eaec72b26e4ffb
antoinegreuzard <antoine@antoinegreuzard.fr>
2023-12-09 10:52:01 +0100

    Refactored webpack configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -5,12 +5,13 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     domains: ['imgix.cosmicjs.com', 'cosmic-s3.imgix.net', 'lh3.googleusercontent.com', 'www.gravatar.com', 'gravatar.com', 'cdn.cosmicjs.com'],
   },
-  webpack: (config, { isServer }) => {
+  webpack: (originalConfig, { isServer }) => {
+    const config = { ...originalConfig }; // Clone the originalConfig into a new object
     if (!isServer) {
       config.experiments = { topLevelAwait: true };
     }
     return config;
-    },
-}
+  },
+};
 
 module.exports = nextConfig;
