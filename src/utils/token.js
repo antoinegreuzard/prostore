3731eec878ed8b2ff12b37c1f09a785958a5991f
Antoine Greuzard <antoine@antoinegreuzard.fr>
2023-12-12 10:42:25 +0100

    Refactor token.js file to use arrow function syntax and remove unnecessary semicolons
    Refactor token.js file to use arrow function syntax and remove unnecessary semicolons

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,25 +1,25 @@
-import { APP_KEY } from './constants/appConstants';
+import { APP_KEY } from './constants/appConstants'
 
-export const setToken = (token) => {
+export const setToken = token => {
   if (token) {
-    localStorage.setItem(APP_KEY, JSON.stringify(token));
+    localStorage.setItem(APP_KEY, JSON.stringify(token))
   }
-};
+}
 
 export const getToken = () => {
-  const tokenLocalStorage = localStorage.getItem(`${APP_KEY}`);
+  const tokenLocalStorage = localStorage.getItem(`${APP_KEY}`)
 
-  return tokenLocalStorage ? JSON.parse(tokenLocalStorage) : null;
-};
+  return tokenLocalStorage ? JSON.parse(tokenLocalStorage) : null
+}
 
 export const removeToken = async () => {
-  localStorage.removeItem(APP_KEY);
+  localStorage.removeItem(APP_KEY)
   try {
-    const response = await fetch('/api/logout', { method: 'POST' });
+    const response = await fetch('/api/logout', { method: 'POST' })
     if (!response.ok) {
-      throw new Error(response.status.toString());
+      throw new Error(response.status.toString())
     }
   } catch (error) {
-    throw new Error(error.toString());
+    throw new Error(error.toString())
   }
-};
+}
